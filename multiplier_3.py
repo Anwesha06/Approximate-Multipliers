1bc1a0d4ed2477a91b8a8611df1b9826e0387959
Anwesha <142707096+Anwesha06@users.noreply.github.com>
2024-10-01 13:53:37 +0530

    Update and rename 4f.py to multiplier_3.py

--- multiplier_3.py
+++ multiplier_3.py
@@ -197,11 +197,11 @@ for i in range(256):
         })
 
 
-with open("4f.txt", "w") as f:
+with open("multiplier_3.txt", "w") as f:
     for r in results:
         f.write(
             f"A={r['A']} B={r['B']} exact={r['exact']} apprx={r['apprx']} #error={r['error']} MED={r['MED']} "
             f"MRED={r['MRED']} MNED={r['MNED']} ER={r['ER']} max={r['max']} MSED={r['MSED']} NMED={r['NMED']} NMSED={r['NMSED']}\n"
         )
 
-print("Simulation complete. Check 4f.txt for results.")
+print("Simulation complete. Check multiplier_3.txt for results.")
